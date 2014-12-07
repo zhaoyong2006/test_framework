97e86a984ece10b3d71d77e1600793d5d736e6e0
simon <pbzyong@126.com>
2014-12-07 21:58:15 +0800

    first commit

---
+++ config/main.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+return array(
+        'control_path' => 'controller',
+        'model_path' => 'model',
+        'view_path' => 'view',
+        'db' => array(
+            'username' => 'root',
+            'password' => '',
+            'port' => 3306,
+            'database' => 'SZframe',
+        ),
+);
